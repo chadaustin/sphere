a5627509d7b6ab15347531c3a8c5c5bde7af7db8
malis <malis@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2001-11-15 02:03:43 +0000

    Added 'about', 'links', 'contact', 'download' sections. Changed many visual features of the site to make it look more appealing to IE users. Next commit will probably involve 'accounts', 'site map', 'projects' etc.
    This was just for temporary use so we can at least get the site in a half-way useful state.
    
    
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@108 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- web/index.php
+++ web/index.php
@@ -3,78 +3,19 @@
   start();
 ?>
 
-  <table class="bodybody">
-    <tr>
-      <td class="bodybody">
         coming soon i guess. (<a href="games/">game list</a>, temporary until i get the database system working)<br /><a href="http://sourceforge.net/projects/sphere">sphere project site</a><br />
 
 <?
   mconnect();
   $QUERY = "SELECT * FROM NEWS_ENTRIES
-              WHERE PARENT = 0";
+              WHERE PARENT = 0
+	      ORDER BY ID DESC";
   $RESULT = mysql_db_query($__DATABASE, $QUERY);
   while($R = mysql_fetch_array($RESULT)){
     news_item($R['ID'], $R['TOPIC'], $R['DATE'], $R['AID'], $R['ICON'], $R['BODY']);
   }
 
   important_links();
-?>
 
-      </td>
-      <td class="sidebar">
-  
-        <table class="sidebaritem">
-          <tr>
-            <td class="sidebaritemhead">
-              news navigation
-            </td>
-          </tr>
-          <tr>
-            <td class="sidebaritembody">
-              20.10.01 - Final Example<br />
-              19.10.01 - More examples<br />
-              18.10.01 - Example News<br />
-              17.10.01 - Examples<br />
-              16.10.01 - Example<br />
-              <br />
-              <div style="text-align: right; width: 100%">older ...</div>
-            </td>
-          </tr>
-        </table>
-  
-        <table class="sidebaritem">
-          <tr>
-            <td class="sidebaritemhead">
-              latest additions
-            </td>
-          </tr>
-          <tr>
-            <td class="sidebaritembody">
-              20.10.01 - Games - Some dumb game<br />
-              19.10.01 - Articles - Some dumb article<br />
-              <br />
-              <div style="text-align: right; width: 100%">complete ...</div>
-            </td>
-          </tr>
-        </table>
-  
-        <table class="sidebaritem">
-          <tr>
-            <td class="sidebaritemhead">
-              my account
-            </td>
-          </tr>
-          <tr>
-            <td class="sidebaritembody">
-              currently, since i don't have access to php and don't really wanna waste my time making an independent file-driven database myself, i don't have a user system. but, when it is implemented, if you're logged in, here you'll be able to edit all sorts of things like which panels on the side you want, possibly what theme you want the site in, if you want to set up a project and what projects you are part of etc. more on this to come hopefully.
-            </td>
-          </tr>
-        </table>
-
-      </td>
-    </tr>
-  </table>
-
-<?
   conclude();
 ?>
--- web/utility.inc.php
+++ web/utility.inc.php
@@ -8,7 +8,7 @@ function start(){
   <html>
     <head>
       <title>sphere.sourceforge.net</title>
-      <link rel="stylesheet" type="text/css" href="http://sphere.sourceforge.net/utility.css" />
+      <link rel="stylesheet" type="text/css" href="http://sphere.sourceforge.net/malis-test/utility.css" />
     </head>
     <body>
       <table cellpadding="0" cellspacing="0" class="main">
@@ -18,29 +18,29 @@ function start(){
             <table cellpadding="0" cellspacing="0" class="topmenu">
               <tr>
                 <td class="menuitems">
-                  <a class="headleft"  href="http://sphere.sourceforge.net/index.php">news</a>
-                  <a class="headleft"  href="http://sphere.sourceforge.net/index.php?U_ACT=articles">articles</a>
-                  <a class="headleft"  href="http://sphere.sourceforge.net/dev.php?U_ACT=current">download</a>
-                  <a class="headleft"  href="http://sphere.sourceforge.net/dev.php?U_ACT=log">dev log</a>
-                  <a class="headleft"  style="border-bottom: none" href="http://sphere.sourceforge.net/index.php?U_ACT=forums">forums</a>
+                  <a class="headleft"  href="http://sphere.sourceforge.net/malis-test/index.php">news</a>
+                  <a class="headleft"  href="http://sphere.sourceforge.net/malis-test/index.php?U_ACT=articles" style="text-decoration: line-through">articles</a>
+                  <a class="headleft"  href="http://sphere.sourceforge.net/malis-test/dev.php?U_ACT=current">download</a>
+                  <a class="headleft"  href="http://sphere.sourceforge.net/malis-test/dev.php?U_ACT=log" style="text-decoration: line-through">dev log</a>
+                  <a class="headleft"  style="border-bottom: none" href="http://aegisknight.org/forum">forums *</a>
                 </td>
 
                 <td class="menulogo">
-                  <img src="http://sphere.sourceforge.net/images/title.png" alt="sphere.sourceforge.net logo" /><br />
-                  <form id="login" method="post" action="http://sphere.sourceforge.net/account.php?U_ACT=login">
+                  <img src="http://sphere.sourceforge.net/images/title.png" alt="sphere.sourceforge.net logo" />
+                  <form id="login" method="post" action="http://sphere.sourceforge.net/malis-test/account.php?U_ACT=login">
                     <div>
                       <input type="text" name="login_name" size="8" />
-                      <a href="javascript:document.getElementById(\'login\').submit()"> login</a> or <a href="http://sphere.sourceforge.net/account.php?U_ACT=register">register</a> (<a href="http://sphere.sourceforge.net/account.php?U_ACT=forgot">forgot password?</a>)
+                      <a href="javascript:document.getElementById(\'login\').submit()"> login</a> or <a href="http://sphere.sourceforge.net/malis-test/account.php?U_ACT=register">register</a> (<a href="http://sphere.sourceforge.net/malis-test/account.php?U_ACT=forgot">forgot password?</a>)
                     </div>
                   </form>
                 </td>
                 
                 <td class="menuitems">
-                  <a class="headright" href="http://sphere.sourceforge.net/projects.php?U_ACT=list">projects</a>
-                  <a class="headright" href="http://sphere.sourceforge.net/dev.php?U_ACT=about">about sphere</a>
-                  <a class="headright" href="http://sphere.sourceforge.net/links.php">links</a>
-                  <a class="headright" href="http://sphere.sourceforge.net/index.php?U_ACT=map">site map</a>
-                  <a class="headright" style="border-bottom: none" href="http://sphere.sourceforge.net/contact.php">contact</a>
+                  <a class="headright" href="http://sphere.sourceforge.net/malis-test/projects.php?U_ACT=list" style="text-decoration: line-through">projects</a>
+                  <a class="headright" href="http://sphere.sourceforge.net/malis-test/dev.php?U_ACT=about">about sphere</a>
+                  <a class="headright" href="http://sphere.sourceforge.net/malis-test/links.php">links</a>
+                  <a class="headright" href="http://sphere.sourceforge.net/malis-test/index.php?U_ACT=map">site map</a>
+                  <a class="headright" style="border-bottom: none" href="http://sphere.sourceforge.net/malis-test/contact.php">contact</a>
                 </td>
               </tr>
             </table>
@@ -49,24 +49,98 @@ function start(){
         </tr>
       <tr>
         <td class="body">
-          THIS SITE IS CURRENTLY UNDERGOING CONSTRUCTION. IT WILL MOST LIKELY HAVE A BILLION ERRORS.';
+          currently under development. line-through means the page is not available yet. (* the forums are the old forums on <a href="http://aegisknight.org">aegisknight.org</a> until the forum code for sphere.sf.net is completed.)
+          <table class="bodybody">
+            <tr>
+              <td class="bodybody">';
 }
 
 
 function conclude(){
   echo '
-          /td>
-	</tr>
+              </td>
+              <td class="sidebar">
+              
+                <table class="sidebaritem">
+                  <tr>
+                    <td class="sidebaritemhead">
+                      news navigation
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="sidebaritembody">
+                      20.10.01 - Final Example<br />
+                      19.10.01 - More examples<br />
+                      18.10.01 - Example News<br />
+                      17.10.01 - Examples<br />
+                      16.10.01 - Example<br />
+                      <br />
+                      <div style="text-align: right; width: 100%">older ...</div>
+                    </td>
+                  </tr>
+                </table>
+                
+                <table class="sidebaritem">
+                  <tr>
+                    <td class="sidebaritemhead">
+                      latest additions
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="sidebaritembody">
+                      20.10.01 - Games - Some dumb game<br />
+                      19.10.01 - Articles - Some dumb article<br />
+                      <br />
+                      <div style="text-align: right; width: 100%">complete ...</div>
+                    </td>
+                  </tr>
+                </table>
+                
+                <table class="sidebaritem">
+                  <tr>
+                    <td class="sidebaritemhead">
+                      my account
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="sidebaritembody">
+                      currently, since i don\'t have access to php and don\'t really wanna waste my time making an independent file-driven database myself, i don\'t have a user system. but, when it is implemented, if you\'re logged in, here you\'ll be able to edit all sorts of things like which panels on the side you want, possibly what theme you want the site in, if you want to set up a project and what projects you are part of etc. more on this to come hopefully.
+                    </td>
+                  </tr>
+                </table>
+                
+              </td>
+            </tr>
+          </table>
+          </td>
+        </tr>
       </table>
     </body>
   </html>';
 }
 
-function news_item($_ID, $_TOPIC, $_DATE, $_AID, $_ICON, $_BODY){
-  $AEMAIL = GetCellFromUID($_AID, 'EMAIL');
-  $AUTHOR = GetCellFromUID($_AID, 'NAME');
+function gen_item( $_HEAD, $_BODY ){
+  echo "
+    <table class=\"item\">
+      <tr>
+        <td class=\"itemhead\">
+          $_HEAD
+        </td>
+      </tr>
+      <tr>
+        <td class=\"itembody\">
+          $_BODY
+          <br />
+        </td>
+      </tr>
+    </table>";
+}
 
-	echo "
+function news_item( $_ID, $_TOPIC, $_DATE, $_AID, $_ICON, $_BODY ){
+  $AEMAIL = GetCellFromUID( $_AID, 'EMAIL' );
+  $AUTHOR = GetCellFromUID( $_AID, 'NAME' );
+  if( $_ICON == 1 ) $_ICON = "announcement";
+    echo "
           <table class=\"item\">
             <tr>
               <td class=\"itemhead\">
@@ -92,7 +166,7 @@ function important_links(){
       <a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
       <a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=873&amp;type=1" alt="sourceforge logo" /></a>
       <br /><span class="smalltext">please note, if you have a user account and are logged in, that this page may not verify with css depending on your theme. in your user preferences, when changing themes, it will be noted if a theme is css valid or not. we encourage you to use <a href="http://w3.org/style/css/">correct css</a> when creating a stylehseet.<br />
-      also, this site most likely has display problems in 4.0 browsers. Use a 6.0 browser and, preferrably, use a browser based off of mozilla :)</span><br />
+      also, this site most likely has display problems in 4.0 browsers. Use a 6.0 browser and, preferrably, use a browser besides Microsoft Internet Explorer 6.0(-) for Windows because it does not allow for alpha blending in PNGs.</span><br />
       <a href="http://kmeleon.org">get k-meleon</a> | <a href="http://mozilla.org">get mozilla</a>
     </div>';
 }
@@ -101,28 +175,28 @@ function mconnect(){ //What can I say? I'm lazy.
   mysql_connect("mysql.sourceforge.net", "sphere", "crothosu") or die("could not connect to the mysql database. contact malis@wolf-phantom.net");
 }
 
-function GetCellFromUID($_ID, $_CELL){
+function GetCellFromUID( $_ID, $_CELL ){
   global $__DATABASE;
   mconnect();
   $QUERY = "SELECT * FROM ACCOUNTS
             WHERE ID=$_ID";
-  $RESULT = mysql_db_query($__DATABASE, $QUERY);
-  while($R = mysql_fetch_array($RESULT)){
+  $RESULT = mysql_db_query( $__DATABASE, $QUERY );
+  while( $R = mysql_fetch_array( $RESULT ) ){
     $RETURN = $R[$_CELL];
   }
-  return($RETURN);
+  return( $RETURN );
 }
 
-function GETUIDFromName($_NAME){
+function GETUIDFromName( $_NAME ){
   global $__DATABASE;
   mconnect();
   $QUERY = "SELECT * FROM ACCOUNTS
             WHERE NAME = \"$_NAME\"";
-  $RESULT = mysql_db_query($__DATABASE, $QUERY);
-  if($R = mysql_fetch_array($RESULT)){
+  $RESULT = mysql_db_query( $__DATABASE, $QUERY );
+  if( $R = mysql_fetch_array( $RESULT ) ){
     $RETURN = $R['ID'];
   }
-  return($RETURN);
+  return( $RETURN );
 }
 
 ?>
