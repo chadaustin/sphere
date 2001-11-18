3f3641a101e2d0afa550874787b180f6ecf651c7
malis <malis@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2001-11-18 09:16:11 +0000

    another mistake. -_- added dev.php, contact.php, and links.php to the cvs tree
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@113 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

---
+++ web/links.php
@@ -0,0 +1,33 @@
+<?
+  include("utility.inc.php");
+  start();
+  
+  gen_item( "sphere-related sites", "
+    <a href=\"http://mastercain.com/flik\">Flik's Site</a><br />
+    <a href=\"http://mastercain.com/wip\">WIP's Site</a><br />
+    <a href=\"http://mastercain.com/drosen\">DRosen's Site</a><br />
+    <a href=\"http://www8.ewebcity.com/fenix159/\">Fenix's Domain</a><br />
+    <a href=\"http://www.mastercain.com/overlord/\">Overlord's Site</a><br />
+    <a href=\"http://www.angelfire.com/biz6/xNASH999\">Nash's Site</a><br />
+    <a href=\"http://rinoa.yi.org/~sugoi/main/info.html\">May-chan's Page</a><br />" );
+  
+  gen_item( "independent game scene", "
+    <a href=\"http://verge-rpg.com/\">VERGE</a><br />
+    <a href=\"http://verge-rpg.com/~tsb\">Ika</a><br />
+    <a href=\"http://mastercain.com\">Mastercain.com</a><br />
+    <a href=\"http://madmonkey.net\">MadMonkey</a><br />
+    <a href=\"http://zophar.net\">Zophar's Domain</a><br />" );
+    
+  gen_item( "helpful resources", "
+    <a href=\"http://developer.netscape.com/docs/manuals/communicator/jsref/index.htm\">Javascript Reference</a><br />
+    <a href=\"http://mathworld.wolfram.com\">World of Mathematics</a><br />
+    <a href=\"http://everything2.com\">everything<span style=\"font-size: 60%\">2</span></a><br />" );
+  
+  gen_item( "other sites", "
+    <a href=\"http://zeromus.org\">Zeromus's Page</a><br />
+    <a href=\"http://falthorn.com\">Falthorn's Page</a>" );
+  
+  important_links();
+  
+  conclude();
+?>
