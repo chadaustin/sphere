5ecef4654b4b4173122f9f0d422dcb1c4d3ae07e
aegis <aegis@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2002-04-24 01:09:21 +0000

    flik's fixes to input.js
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@183 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/system/scripts/input.js
+++ sphere/system/scripts/input.js
@@ -3,11 +3,14 @@ function GetString(x, y, font)
   var background = GrabImage(0, 0, GetScreenWidth(), GetScreenHeight());
   
   var str = "";
-  
+  var cursor_position = 0;
+
   while (true) {
     
     background.blit(0, 0);
-    font.drawText(x, y, str + "_");
+    font.drawText(x, y, str);
+    font.drawText(x + font.getStringWidth(str.slice(0, cursor_position), cursor_position), y, "|");
+
     FlipScreen();
     
     while (AreKeysLeft()) {
@@ -16,19 +19,38 @@ function GetString(x, y, font)
       switch (key) {
       
         // done
-        case KEY_ENTER: {
+        case KEY_ENTER: { 
           return str;
         }
         
         // backspace
         case KEY_BACKSPACE: {
           if (str != "") {
-            str = str.substring(0, str.length - 1);
+           str = str.slice(0, cursor_position - 1) + str.slice(cursor_position + 1);
           }
+          if (cursor_position > 0)
+            cursor_position -= 1;
+          break;
+        }
+
+        case KEY_LEFT: {
+           if (cursor_position > 0)
+             cursor_position -= 1;
+          break;
+        }
+
+        case KEY_RIGHT: {
+          if (cursor_position <= str.length - 1)
+            cursor_position += 1;
+          break;
         }
         
         default: {
-          str += GetKeyString(key, IsKeyPressed(KEY_SHIFT));
+          var shift = IsKeyPressed(KEY_SHIFT);
+          if (GetKeyString(key, shift) != "") {
+            str = str.slice(0, cursor_position) + GetKeyString(key, shift) + str.slice(cursor_position);
+            cursor_position += 1;
+          }
         }
       } // end switch
       
