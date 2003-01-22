0fa0d215db3b51078ce9e78c001543f206c9e5b3
aegis <aegis@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2003-01-22 08:16:33 +0000

    add an installer
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@372 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

---
+++ sphere/make-installer.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+die() {
+    echo "Aborting..."
+    exit 1;
+}
+
+BUILD=build/vc6
+INSTALL=installer
+DIRS="docs games startup system util"
+
+echo "Building Sphere..."
+scons compiler=vc6 opt=1
+
+echo "Cleaning old files..."
+rm -rf $INSTALL/*.exe || die
+rm -rf $INSTALL/*.dll || die
+for d in $DIRS; do
+    rm -rf $INSTALL/$d || die
+done
+
+echo "Copying files from $BUILD to $INSTALL..."
+cp -R $BUILD/*.exe $INSTALL || die
+cp -R $BUILD/*.dll $INSTALL || die
+for d in $DIRS; do
+    cp -R $BUILD/$d $INSTALL || die
+done
+
+echo "Removing unnecessary generated files..."
+(find $INSTALL -name .sconsign | xargs rm -f) || die
+
+echo "Building installer..."
+(cd $INSTALL && cygstart -a "compile-bz2" sphere.nsi) || die
+
+echo "Done"
