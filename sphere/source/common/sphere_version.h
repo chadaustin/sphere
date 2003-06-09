0e8bb7f1d4a9d0f075ca8a21d1bcc4c9dcf15c66
gau_veldt <gau_veldt@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2003-06-09 06:55:35 +0000

    Made small version number increase.
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@640 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/source/common/sphere_version.h
+++ sphere/source/common/sphere_version.h
@@ -4,10 +4,10 @@
 
 // return the Sphere version as a floating point number
 inline double GetSphereVersion() {
-  return 1.1;
+  return 1.101;
 }
 
-#define SPHERE_VERSION "v1.1"
+#define SPHERE_VERSION "v1.101"
 
 
 #endif
