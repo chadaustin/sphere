fc1e5d21a28936c6224409cd821f117a0b0f187b
flik9x <flik9x@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2004-02-15 19:18:08 +0000

    Updating old_spriteset.js so GetPersonDirection works
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@1024 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/system/scripts/old_spriteset.js
+++ sphere/system/scripts/old_spriteset.js
@@ -1,24 +1,35 @@
+///////////////////////////////////////////////////////////
 
 function Spriteset(spriteset) {
   this.spriteset = spriteset;
 }
 
+///////////////////////////////////////////////////////////
+
 Spriteset.prototype.getFrame = function(direction_index, frame_index) {
   return (this.spriteset.images[this.spriteset.directions[direction_index].frames[frame_index].index]);
 }
+
+///////////////////////////////////////////////////////////
 
 Spriteset.prototype.getNumFrames = function(direction_index){
   // returns total frames for the direction index
   return (this.spriteset.directions[direction_index].frames.length);
 }
 
+///////////////////////////////////////////////////////////
+
 Spriteset.getNumDirections = function() {
   return (this.spriteset.directions.length);
 }
 
+///////////////////////////////////////////////////////////
+
 var OldLoadSpriteset      = LoadSpriteset;
 var OldSetPersonDirection = SetPersonDirection;
 var OldGetPersonDirection = GetPersonDirection;
+
+///////////////////////////////////////////////////////////
 
 function init_old_spriteset_code() {
   LoadSpriteset = function(filename) {
@@ -32,10 +43,15 @@ function init_old_spriteset_code() {
   GetPersonDirection = function(name) {
     var direction = OldGetPersonDirection(name);
     for (var i = 0, spriteset = GetPersonSpriteset(name); i < spriteset.directions.length; ++i)
-      if (spriteset.directions[i].name == name)
-        return (i);
+      if (spriteset.directions[i].name == direction)
+        return (i);       
+    return 0;
   }
 
 }
 
+///////////////////////////////////////////////////////////
+
 init_old_spriteset_code();
+
+///////////////////////////////////////////////////////////
