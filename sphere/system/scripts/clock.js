6e6e68b9e88796c7d62532f94fd794f22656010a
tunginobi <tunginobi@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2006-09-30 09:07:29 +0000

    Fixed bug in clock.js system script, causing it to work incorrectly when offset time is loaded.
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@1231 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/system/scripts/clock.js
+++ sphere/system/scripts/clock.js
@@ -1,4 +1,9 @@
 // Game clock object by WIP
+
+// Modified slightly by tunginobi to correctly show time
+// after the hours, minutes and/or seconds have been
+// changed or loaded.
+
 // This is a function that will create a game clock object.
 // Just use the function clock.getTime() and it will
 // return an object with seconds, minutes and hours
@@ -7,58 +12,52 @@
 
 function Clock()
 {
-	this.started = 0;
-	this.seconds = 0;
-	this.minutes = 0;
-	this.hours = 0;
+  this.started = 0;
+  this.seconds = 0;
+  this.minutes = 0;
+  this.hours = 0;
 }
 
 // Example: var GameClock = new Clock();
 
 Clock.prototype.start = function()
 {
-	this.started = GetTime();
+  this.started = GetTime();
 }
 
 // Example: GameClock.start();
 
 Clock.prototype.getTime = function(a)
 {
-	var current = GetTime() - this.started;
-	var secs = "";
-	var mins = "";
-	var hours = "";
-	if (Math.floor((current)/1000 + this.seconds) % 60 < 10)
-	{
-		secs += "0";
-	}
-	secs += Math.floor((current)/1000 + this.seconds) % 60;
-	if (Math.floor((current)/1000/60 + this.minutes) % 60 < 10)
-	{
-		mins += "0";
-	}
-	mins += Math.floor((current)/1000/60 + this.minutes) % 60;
-	if (Math.floor((current)/1000/60/60 + this.hours) % 60 < 100)
-	{
-		hours += "0";
-	}
-	if (Math.floor((current)/1000/60/60 + this.hours) % 60 < 10)
-	{
-		hours += "0";
-	}
-	hours += Math.floor((current)/1000/60/60 + this.hours) % 60;
-	if (a == true)
-	{
-		return [hours, mins, secs];
-	}
-	else
-		return hours + ":" + mins + ":" + secs;
+  var current = GetTime() - this.started;
+  var secs = "";
+  var mins = "";
+  var hours = "";
+  
+  if (Math.floor((current)/1000 + this.seconds) % 60 < 10)
+    secs += "0";
+  secs += Math.floor((current)/1000 + this.seconds) % 60;
+  
+  if (Math.floor((current + this.seconds * 1000)/1000/60 + this.minutes) % 60 < 10)
+    mins += "0";
+  mins += Math.floor((current + this.seconds * 1000)/1000/60 + this.minutes) % 60;
+  
+  if (Math.floor((current + this.seconds * 1000 + this.minutes * 60 * 1000)/1000/60/60 + this.hours) % 60 < 100)
+    hours += "0";
+  if (Math.floor((current + this.seconds * 1000 + this.minutes * 60 * 1000)/1000/60/60 + this.hours) % 60 < 10)
+    hours += "0";
+  hours += Math.floor((current + this.seconds * 1000 + this.minutes * 60 * 1000)/1000/60/60 + this.hours) % 60;
+  
+  if (a == true)
+    return [hours, mins, secs];
+  else
+    return hours + ":" + mins + ":" + secs;
 }
 
 // Example:
 // font.drawText(10, 10, clock.getTime());
 //
-// Every time you want to start keeping track of time, 
+// Every time you want to start keeping track of time,
 // start the clock. If you are loading a game, you should
 // load the time into the clock object. That way, it can keep
 // correct time with a loaded game.
