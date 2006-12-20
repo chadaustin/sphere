dea926a3ca8b1c7c3f8d6456484f1ca716b0badb
tunginobi <tunginobi@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2006-12-20 22:02:59 +0000

    Set version to 1.13, after frame rate function bugfix.
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@1249 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/source/common/sphere_version.h
+++ sphere/source/common/sphere_version.h
@@ -4,8 +4,8 @@
 // return the Sphere version as a floating point number
 inline double GetSphereVersion()
 {
-    return 1.12;
+    return 1.13;
 }
 
-#define SPHERE_VERSION "v1.12"
+#define SPHERE_VERSION "v1.13"
 #endif
