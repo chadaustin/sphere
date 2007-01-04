ef9b5c446695f845ca780a99c4f84f74b61438c1
tunginobi <tunginobi@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2007-01-04 18:51:59 +0000

    Total script redesign. It works a lot better now.
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@1263 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/system/scripts/named_parameters.js
+++ sphere/system/scripts/named_parameters.js
@@ -1,212 +1,101 @@
 /**
  * named_parameters.js
  *
- * Pass parameters to functions by name, in any order.
- * Handy for more readable code.
+ * Allow passing parameters to a function by name.
+ * See docs/system_scripts/named_parameters.txt for more details.
  *
- * API:
- * Function.setParameters(in_params, required_template[, optional_template])
- * Object.mergeStrict(other_object)
- * Object.mergeLoose(other_object)
- * 
- * Exceptions:
- * MissingArgumentException
- * IllegalArgumentException
- * BadMergeException
- *
- * Usage:
- * In the function where you want named parameters:
- * 
- *   function drawText(params) {
- *     var data = Function.setParameters(params, {text: ""}, {title: "", position: {x: 2, y: 2}});
- *     data.position.mergeStrict(params.position);
- *     // ...
- *     // Code that uses data.text, data.title, data.position.x, data.position.y
- *   }
- * 
- * Then call it in any of these ways. Note the use of the literal object:
- * 
- *   drawText({text: "Hey guys, what's going on?"});
- *   drawText({title: "Bobby", text: "Nothing much."});
- *   drawText({text: "Hahahahaha!", position: {x: 5, y: 20}});
- * 
- * All functions ignore nested object parameters, so you have to set them
- * manually using Object.mergeStrict() or Object.mergeLoose(), as shown
- * in the example above.
- * 
- * N.B.: Any Sphere objects will still have to be checked manually:
- * 
- *   function drawWindow(params) {
- *     // Stuff from above
- *     // ...
- *     if (!params.windowStyle
- *         || typeof params.windowStyle != "[object windowstyle]")
- *       throw "IllegalArgumentException: blah blah blah...";
- *     data.windowStyle = params.windowStyle;
- *     // ...
- *     // Code that uses data.windowStyle, etc.
- *   }
- *
- * @author	Tung Nguyen
+ * @author	tunginobi
  */
 
-/**
- * Checks parameters of an input object against given templates,
- * and merges them into a single object. Ignores object members.
- * 
- * @param	in_params	Object containing named input parameters.
- * @param	required_template	A template object whose members must
- * 	exist in the in_params object.
- * @param	optional_template	A template object whose members may be
- * 	overridden by those in the in_params object.
- * @throws	MissingArgumentException	in_params does not have a
- * 	member that required_template needs.
- * @throws	IllegalArgumentException	in_params has a member that
- * 	doesn't exist in either template, or one that does not match types with
- *	the template requirements.
- * @return	A single object containing all members of the input objects,
- * 	with in_param's members overriding defaults as needed.
- */
-Function.prototype.setParameters = function (in_params, required_template, optional_template)
+Function.prototype.setParameters = function (params, template, strict)
 {
-	if (!in_params)
-		throw "MissingArgumentException: Function.setParameters() requires in_params "
-				+ "argument.";
-	if (!required_template)
-		throw "MissingArgumentException: Function.setParameters() requires "
-				+ "required_template argument.";
+	function CopyObject(data_in, obj)
+	{
+		for (var o in obj)
+		{
+			if (typeof obj[o] === "object")
+				CopyObject(data_in[o], obj[o]);
+			else
+				data_in[o] = obj[o];
+		}
+	}
 	
-	// Fill in default values
+	function CopyTemplate(data_in, tmp)
+	{
+		var req = tmp["req!"];
+		var opt = tmp["opt!"];
+		for (var r in req)
+		{
+			if (typeof req[r] === "object")
+			{
+				data_in[r] = {};
+				CopyTemplate(data_in[r], req[r]);
+			}
+			else
+				data_in[r] = req[r];
+		}
+		for (var o in opt)
+		{
+			if (typeof opt[o] === "object")
+			{
+				data_in[o] = {};
+				CopyTemplate(data_in[o], opt[o]);
+			}
+			else
+				data_in[o] = opt[o];
+		}
+	}
+	
+	if ("req!" in template === false)
+		throw "InvalidTemplateException: Function.setParameters()"
+				+ " could not find template member \"req!\".";
+	if ("opt!" in template === false)
+		throw "InvalidTemplateException: Function.setParameters()"
+				+ " could not find template member \"opt!\".";
+	
+	// Set defaults
 	var data = {};
-	for (var r in required_template)
-	{
-		if (typeof required_template[r] != "object"
-				&& (typeof required_template[r]).indexOf("[object") != 0)
-		{
-			if (r in in_params == false)
-				throw "MissingArgumentException: Function.setParameters() requires "
-						+ "in_params to have member \"" + r +"\".";
-			data[r] = required_template[r];
-		}
-	}
-	for (var o in optional_template)
-	{
-		if (typeof required_template[o] != "object"
-				&& (typeof required_template[o]).indexOf("[object") != 0)
-		{
-			data[o] = optional_template[o];
-		}
-	}
+	CopyTemplate(data, template);
 	
-	for (var i in in_params)
+	if (strict)
 	{
-		if (typeof in_params[i] != "object"
-				&& (typeof in_params[i]).indexOf("[object") != 0)
+		// Cross check params's members against the required ones
+		for (var r in template["req!"])
 		{
-			if (i in required_template)
-			{
-				if (typeof in_params[i] == typeof required_template[i])
-					data[i] = in_params[i];
-				else
-					throw "IllegalArgumentException: Function.setParameters() "
-							+ "detected mismatched in_params/required_template "
-							+ "member \"" + i + "\".";
-			}
-			else if (i in optional_template)
-			{
-				if (typeof in_params[i] == typeof optional_template[i])
-					data[i] = in_params[i];
-				else
-					throw "IllegalArgumentException: Function.setParameters() "
-							+ "detected mismatched in_params/optional_template "
-							+ "member \"" + i + "\".";
-			}
-			else
-			{
-				throw "IllegalArgumentException: Function.setParameters() could not find "
-						+ "a matching member \"" + i + "\" in required/optional_template "
-						+ "from in_params.";
-			}
+			if (r in params === false)
+				throw "MissingArgumentException: Function.setParameters()"
+						+ " could not find required argument \"" + r + "\".";
 		}
-		else
+		
+		// Cross check params's members against all parameters and transfer.
+		for (var p in params)
 		{
-			if (i in required_template)
+			if (p in template["req!"] || p in template["opt!"])
 			{
-				if (typeof in_params[i] != typeof required_template[i])
-					throw "IllegalArgumentException: Function.setParameters() "
-							+ "detected mismatched in_params/required_template "
-							+ "member \"" + i + "\".";
-			}
-			else if (i in optional_template)
-			{
-				if (typeof in_params[i] != typeof optional_template[i])
-					throw "IllegalArgumentException: Function.setParameters() "
-							+ "detected mismatched in_params/optional_template "
-							+ "member \"" + i + "\".";
+				var which = "opt!";
+				if (p in template["req!"])
+					which = "req!";
+				if (typeof params[p] !== typeof template[which][p])
+				{
+					throw "IllegalArgumentException: Function.setParameters()"
+							+ " type mismatch for argument \"" + p + "\";"
+							+ " expected " + typeof template[which][p] + ","
+							+ " found " + typeof params[p] + ".";
+				}
+				else if (typeof params[p] === "object")
+					data[p] = Function.setParameters(params[p], template[which][p], strict);
+				else
+					data[p] = params[p];
 			}
 			else
 			{
-				throw "IllegalArgumentException: Function.setParameters() could not find "
-						+ "a matching member \"" + i + "\" in required/optional_template "
-						+ "from in_params.";
+				throw "IllegalArgumentException: Function.setParameters()"
+						+ " found stray argument \"" + p + "\".";
 			}
 		}
 	}
+	else
+		CopyObject(data, params);
 	
 	return data;
-}
-
-/**
- * Takes an object with exact matching members, and adopts all of them.
- * Object members are ignored.
- *
- * @param	other_object	Object from which to take member data.
- * @throws	BadMergeException	One object doesn't have a member that
- * 	the other one needs.
- */
-Object.prototype.mergeStrict = function (other_object)
-{
-	if (!other_object || typeof other_object != "object")
-		throw "BadMergeException: Object.mergeStrict() requires an object input argument.";
-	
-	for (var o in other_object)
-	{
-		if (o in this == false)
-			throw "BadMergeException: Object.mergeStrict() found unexpected member \""
-					+ o + "\" in passed argument.";
-	}
-	
-	for (var t in this)
-	{
-		if (t in other_object)
-		{
-			if (typeof this[t] != "object"
-					&& (typeof this[t]).indexOf("[object") != 0)
-				this[t] = other_object[t];
-		}
-		else
-		{
-			throw "BadMergeException: Object.mergeStrict() argument is missing required "
-					+ "member \"" + t + "\".";
-		}
-	}
-}
-
-/**
- * Adopts the members of an input object, having its own members overridden as
- * needed. Object members are ignored.
- *
- * @param	other_object	Object from which to take member data.
- */
-Object.prototype.mergeLoose = function (other_object)
-{
-	if (other_object)
-	{
-		for (var i in other_object)
-		{
-			if (typeof other_object[i] != "object"
-					&& (typeof other_object[i]).indexOf("[object") != 0)
-				this[i] = other_object[i];
-		}
-	}
 }
