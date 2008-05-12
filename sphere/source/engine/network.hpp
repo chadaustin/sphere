bfe5050626c8659d05245075c44409f2efc3cfe2
kyuu <kyuu@c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2>
2008-05-12 20:13:27 +0000

    Removed the obsolete and buggy SDL subsystem.
    git-svn-id: file:///desktop/SourceForge%20Backup/sphere/svnrepos/trunk@1360 c0eb2ce6-7f40-480a-a1e0-6c4aea08c2c2

--- sphere/source/engine/network.hpp
+++ sphere/source/engine/network.hpp
@@ -1,11 +1,13 @@
-#if   defined(SPHERE_SDL)
-#include "sdl/sdl_network.hpp"
-#elif defined(MAC)
+#if   defined(MAC)
 #include "mac/mac_network.h"
+
 #elif defined(WIN32)
 #include "win32/win32_network.hpp"
+
 #elif defined(unix)
 #include "unix/unix_network.h"
+
 #else
 #error unsupported platform
+
 #endif
--- sphere/source/engine/time.hpp
+++ sphere/source/engine/time.hpp
@@ -1,11 +1,13 @@
-#if   defined(SPHERE_SDL)
-#include "sdl/sdl_time.hpp"
-#elif defined(MAC)
+#if   defined(MAC)
 #include "mac/mac_time.h"
+
 #elif defined(WIN32)
 #include "win32/win32_time.hpp"
+
 #elif defined(unix)
 #include "unix/unix_time.h"
+
 #else
 #error unsupported platform
+
 #endif
